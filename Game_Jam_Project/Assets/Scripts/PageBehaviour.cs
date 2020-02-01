5a0065a8ea453f8f1f312409ea28d68092a245d1
Cby <jorisgonc@hotmail.fr>
2020-02-01 10:44:23 +0100

    ModifScript Grimoire

---
+++ Game_Jam_Project/Assets/Scripts/PageBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PageBehaviour : MonoBehaviour
+{
+    public void ActivateVisual()
+    {
+    if (gameObject.GetComponent<Image>() != null)
+        {
+            gameObject.GetComponent<Image>().enabled = true;
+        
+        }
+    }
+
+    public void DesactivateVisual()
+    {
+        if (gameObject.GetComponent<Image>() != null)
+        {
+            gameObject.GetComponent<Image>().enabled = false;
+        }
+    }
+}
