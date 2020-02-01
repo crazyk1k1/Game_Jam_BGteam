731c096379b2596744e3122487712be5d4010851
Alkeist <steeve.leclech@gmail.com>
2020-02-01 19:34:54 +0100

    Script Updated

---
+++ Game_Jam_Project/Assets/Scripts/SteeveLC/Core/Monobehaviour/Object/TargetBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TargetBehaviour : MonoBehaviour
+{
+    public ObjectData targetWordSocket;
+    public ObjectData targetVerbSocket;
+
+    public GameEvent targetEvent;
+
+
+    public void ActivateNextStep()
+    {
+        targetEvent.Raise();
+    }
+}
