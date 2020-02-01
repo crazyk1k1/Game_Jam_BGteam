86f434cf5996408b15304f958ba8280dd909e0ba
Alkeist <steeve.leclech@gmail.com>
2020-02-01 08:36:03 +0100

    Ajout base GameEvents

---
+++ Game_Jam_Project/Assets/Scripts/SteeveLC/Core/Monobehaviour/Events/DataEvents/DataTransferEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ [CreateAssetMenu]
+public class DataTransferEvent : ScriptableObject
+{
+    /// <summary>
+    /// The list of listeners that this event will notify if it is raised.
+    /// </summary>
+    private readonly List<DataTransferEventListner> eventListeners = 
+        new List<DataTransferEventListner>();
+
+        [HideInInspector]public  GameObject dataCarried; // CHANGER ICI LE TYPE DE DATA 2/3
+
+
+    public void Raise(GameObject dataTransfer) // CHANGER ICI LE TYPE DE DATA 3/3
+    {
+        dataCarried = dataTransfer;
+        for(int i = eventListeners.Count -1; i >= 0; i--)
+            eventListeners[i].OnEventRaised();
+    }
+
+
+    public void RegisterListener(DataTransferEventListner listener)
+    {
+        if (!eventListeners.Contains(listener))
+            eventListeners.Add(listener);
+    }
+
+    public void UnregisterListener(DataTransferEventListner listener)
+    {
+        if (eventListeners.Contains(listener))
+            eventListeners.Remove(listener);
+    }
+}
---
+++ Game_Jam_Project/Assets/Scripts/SteeveLC/Core/Monobehaviour/Events/DataEvents/DataTransferResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class DataTransferResponse : UnityEvent<GameObject> // changer Ici le type de data 1/3
+{
+
+}
+
