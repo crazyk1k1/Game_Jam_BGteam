ad39683c964bd4675848a5ad3c956f838e248eff
Zetsubo9 <max.lebolloch@laposte.net>
2020-02-01 17:32:24 +0100

    respawn

---
+++ Game_Jam_Project/Assets/Scripts/PlayerScripts/PlayerRespawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+
+    public Vector3 respawnPoint;
+    public bool teleport;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPoint = GameObject.Find("RespawnPoint").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -150f)
+        {
+            teleport = true;
+            gameObject.GetComponent<CharacterController>().enabled = false;
+        }
+        if (teleport)
+        {
+            gameObject.transform.position = respawnPoint;
+            gameObject.GetComponent<CharacterController>().enabled = true;
+            teleport = false;
+        }
+    }
+}
