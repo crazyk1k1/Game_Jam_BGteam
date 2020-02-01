099416fc5d7690c3874c9935127ff4ee1f5d52ea
Zetsubo9 <max.lebolloch@laposte.net>
2020-02-01 02:32:09 +0100

    grab

--- Game_Jam_Project/Assets/Scripts/WordGrab.cs
+++ Game_Jam_Project/Assets/Scripts/WordGrab.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class WordGrab : MonoBehaviour
 {
+
+    public bool isMoving;
+    public List<GameObject> words;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,38 @@ public class WordGrab : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray;
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            {
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    if (hit.collider.gameObject.name == "Cube")
+                    {
+                        words.Add(hit.collider.gameObject);
+                        isMoving = true;
+                    }
+                }
+            }
+        }
+
+        if (isMoving)
+        {
+            Grab();
+        }
     }
+
+    public void Grab()
+    {
+        words[0].transform.position = Vector3.MoveTowards(words[0].transform.position, gameObject.transform.position, 0.5f);
+        if(Vector3.Distance(words[0].transform.position, gameObject.transform.position) <= 1f)
+        {
+            words[0].GetComponent<MeshRenderer>().enabled = false;
+            words[0].GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
 }
