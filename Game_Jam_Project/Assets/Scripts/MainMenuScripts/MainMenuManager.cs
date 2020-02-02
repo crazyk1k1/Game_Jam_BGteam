e388c96a166b0e34d6d79f0e63fb5d4e91e9313e
Zetsubo9 <max.lebolloch@laposte.net>
2020-02-02 12:15:53 +0100

    MainMenu & Ending en cours

--- Game_Jam_Project/Assets/Scripts/MainMenuScripts/MainMenuManager.cs
+++ Game_Jam_Project/Assets/Scripts/MainMenuScripts/MainMenuManager.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class MainMenuManager : MonoBehaviour
 {
     public GameObject FonduLeave;
+    public GameObject FonduEnter;
+    public GameObject mainMenuSection;
+    public GameObject helpSection;
     public float timer;
+    public float enterTimer;
     public bool goTimer;
+    public bool onHelpMenu;
+    public bool goPlay;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +23,13 @@ public class MainMenuManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        enterTimer += 1 * Time.deltaTime;
+
+        if(enterTimer >= 2)
+        {
+            FonduEnter.SetActive(false);
+        }
         if (goTimer)
         {
             timer += 1 * Time.deltaTime;
@@ -25,10 +38,33 @@ public class MainMenuManager : MonoBehaviour
         {
             FonduLeave.SetActive(true);
         }
+
+        if (onHelpMenu)
+        {
+            mainMenuSection.SetActive(false);
+            helpSection.SetActive(true);
+        }
+        else
+        {
+            mainMenuSection.SetActive(true);
+            helpSection.SetActive(false);
+        }
+        if (goPlay)
+        {
+            mainMenuSection.SetActive(false);
+        }
     }
 
     public void SetFondu()
     {
         goTimer = true;
+        goPlay = true;
     }
+
+    public void HelpMenu()
+    {
+        onHelpMenu = !onHelpMenu;
+    }
+
+
 }
---
+++ Game_Jam_Project/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+
+    public Transform respawn;
+    public bool canTeleport;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawn = GameObject.Find("SpawnPoint").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y <= -200)
+        {
+            canTeleport = true;
+            gameObject.GetComponent<CharacterController>().enabled = false;
+        }
+        if (canTeleport)
+        {
+            transform.position = respawn.position;
+            gameObject.GetComponent<CharacterController>().enabled = true;
+            canTeleport = false;
+        }
+    }
+}
